889ef03d860acab06376b79b4e8612d30a021a3d
zjh <zjh@192.168.3.9>
2019-04-18 01:05:05 +0800

    建立 springBoot 專案

---
+++ src/main/java/idv/zjh/vxeasyui/VxEasyuiApplication.java
@@ -0,0 +1,13 @@
+package idv.zjh.vxeasyui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VxEasyuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VxEasyuiApplication.class, args);
+	}
+
+}
