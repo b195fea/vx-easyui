bbb366c3c55dd545d7ab8e3fa1302a24bbc7665b
terence.chung <b195fea@gmail.com>
2019-04-18 15:31:17 +0800

    設立第一個測試頁

--- src/main/java/idv/zjh/vxeasyui/ctrl/PageController.java
+++ src/main/java/idv/zjh/vxeasyui/ctrl/PageController.java
@@ -5,30 +5,27 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import idv.zjh.vxeasyui.model.Person;
 import idv.zjh.vxeasyui.service.IIndex;
 
 @Controller
-public class MainController {
+public class PageController {
 	
-	@Autowired
-	private IIndex service;
-
 	@RequestMapping("/")
 	public ModelAndView index() {
 		Map<String, String> map = new HashMap<String, String>();
-		map.put("name", "Thymeleaf1651616sagsgsgsfagsag5165");
+		map.put("name", "首頁");
 		return new ModelAndView("index", map);
 	}
 	
-	@RequestMapping("/model")
-	@ResponseBody
-	public Person getModel() {
-		return service.getModel();
+	/**
+	 * vx-easyUI Demo
+	 * @return
+	 */
+	@RequestMapping("/vue/page1")
+	public ModelAndView vxeasyuiDemo() {
+		return new ModelAndView("vue/page1");
 	}
 }
---
+++ src/main/resources/static/js/vue/page1.js
@@ -0,0 +1,3 @@
+/**
+ * EasyUI Demo
+ */
