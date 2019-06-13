27c3cd373359267c5a19460e538a836b071bcb1a
terence.chung <b195fea@gmail.com>
2019-06-13 14:37:35 +0800

    各類設定檔案

---
+++ src/main/resources/static/js/vue/page2.js
@@ -0,0 +1,14 @@
+/*class page2{
+	constructor(){
+		this.name = "name";
+	}
+	
+	getName(){
+		return this.name;
+	}
+}
+
+export page2;*/
+export var firstName = 'Michael';
+export var lastName = 'Jackson';
+export var year = 1958;
